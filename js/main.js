67671c71ca3ce676d9778c3385fadfd81372a54f
omaracrystal <omaracrystal@gmail.com>
2015-08-07 14:07:32 -0600

    created files for tic tac toe

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Constructor
+
+// Create a Game() class that creates instances of the Board() class, Players() class (you need two players) and has a turnCounter property to indicate whose turn it is and determine the winner in case of a draw.
+
+function Game() {
+  this.board = new Board()
+  this.player1 = new Player()
+  this.player2 = new Player()
+  this.turnCounter = turnCounter;
+}
+
+function Board() {
+  this
+}
